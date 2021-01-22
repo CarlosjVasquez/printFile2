ecffdd7a9d24e054594ff54b60f93b8a9a5bc303
CarlosjVasquez <virahondacarlos@gmail.com>
2021-01-22 15:28:22 -0500

    printFile

--- printFile.py
+++ printFile.py
@@ -19,24 +19,31 @@ def download():
 
     url = request.args.get('url')
     username = request.args.get('username')
+    name = request.args.get('name')
     destino ='C:/Users/USUARIO/Desktop/Trabajo/ProyectoLibertad/printFile/pdf/' + str(username)
     if not os.path.exists(destino):
         os.makedirs(destino)
-    download = wget.download(url, destino)
+    namefinish = destino+ '/' + str(name) + '.pdf'
+    download = wget.download(url, namefinish)
 
     return download
 
 
 
-@app.route("/print/<nombre>",methods=['GET'])
-def print(nombre):
+@app.route("/print/",methods=['GET'])
+def print():
 
-    return_code = subprocess.call(["PDFtoPrinter", nombre ,"PDFCreator"], shell=True) 
+    name = request.args.get('name')
+    username = request.args.get('username')
+
+    url = 'C:\\Users\\USUARIO\\Desktop\\Trabajo\\ProyectoLibertad\\printFile\\pdf\\' + str(username) +'\\' + str(name) + '.pdf'
+
+    return_code = subprocess.call(["PDFtoPrinter", url ,"PDFCreator"], shell=True) 
 
     if(return_code != 0):
         return 'Error'
     else: 
-        return 'Ok'
+        return url
         
 
 if __name__ == "__main__":
